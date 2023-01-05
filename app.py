0f3ecb33d2ff4101281f155e2e02fe212b43f52a
Wilk Michaël <114574249+MickaWilk@users.noreply.github.com>
2023-01-05 23:47:01 +0100

    Update app.py
    Affichage des images des recherches

--- app.py
+++ app.py
@@ -1,24 +1,33 @@
 import streamlit as st
 import requests
+import re
+CATEGORY = {"people": "characters", "planets": "planets", "starships": "starships"}
 
+# Recherche de l'id
+
+def parse_url_id(url):
+  item_id_match = re.search(r'/api/\w+/(\d+)/', url)
+  if item_id_match:
+    item_id = item_id_match.group(1)
+    print(f"L'identifiant de l'élément est {item_id}.")
+    return item_id
+  else:
+    print("Impossible de trouver l'identifiant de l'élément.")
+    
+    
+    
 # Fonction pour réaliser une recherche avec l'API SWAPI
 def search(category, query):
-  # Construire l'URL de l'API en fonction de la catégorie et de la requête
   url = f"https://swapi.dev/api/{category}?search={query}"
-
-  # Effectuer une requête GET à l'API
   response = requests.get(url)
-
-  # Extraire les résultats de la réponse
   results = response.json()['results']
-
   return results
 
-# Créer un menu déroulant pour sélectionner la catégorie de recherche
+
+# Streamlit part
+
 st.sidebar.title("Options de recherche")
 category = st.sidebar.selectbox("Choisissez une catégorie", ["people", "planets", "starships"])
-
-# Créer un champ de saisie pour entrer la requête de recherche
 query = st.sidebar.text_input("Entrez votre recherche")
 
 # Si l'utilisateur a saisi une requête, effectuer la recherche et afficher les résultats
@@ -27,5 +36,8 @@ if query:
   st.title(f"Résultats de la recherche pour '{query}' dans la catégorie '{category}' :")
   for result in results:
     st.markdown(f"- **{result['name']}**")
-    if 'image' in result:
-        st.image(result['image'])
+    id = parse_url_id(result['url'])
+    if requests.get(f"https://starwars-visualguide.com/assets/img/{CATEGORY[category]}/{id}.jpg").status_code == 200:
+      st.image(f"https://starwars-visualguide.com/assets/img/{CATEGORY[category]}/{id}.jpg")
+    else:
+      st.image(f"https://starwars-visualguide.com/assets/img/placeholder.jpg")
